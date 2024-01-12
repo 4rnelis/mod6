7a067d8541e20245a60dcd08b28a00db3d5df678
s3071081 <e.jotautas@utwente.nl>
2024-01-12 20:52:01 +0100

    new design

--- MAIN/gallery-script.js
+++ MAIN/gallery-script.js
@@ -3,30 +3,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const backButton = document.getElementById('back-button');
     const continueButton = document.getElementById('continue-button');
 
-    // Retrieve captured images from localStorage
     let capturedImages = JSON.parse(localStorage.getItem('capturedImages')) || [];
 
-    // Display captured images in the gallery container
     capturedImages.forEach((imageDataURL, index) => {
         const imageElement = createImageElement(imageDataURL, index);
         galleryContainer.appendChild(imageElement);
     });
 
-    // Add click event listener to the back button
     backButton.addEventListener('click', () => {
-        // Update captured images in localStorage
         localStorage.setItem('capturedImages', JSON.stringify(capturedImages));
-
-        // Navigate back to the camera screen
         window.location.href = 'index.html';
     });
 
-    // Add click event listener to the continue button
     continueButton.addEventListener('click', () => {
-        // Update captured images in localStorage
         localStorage.setItem('capturedImages', JSON.stringify(capturedImages));
 
-        // Navigate to the edit page
         window.location.href = 'edit.html'; // Change 'edit.html' to the actual edit page
     });
 
@@ -49,21 +40,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     function removeImage(index) {
-        // Remove the image from the capturedImages array
         capturedImages.splice(index, 1);
 
-        // Update the gallery container
         updateGallery();
 
-        // Update captured images in localStorage
         localStorage.setItem('capturedImages', JSON.stringify(capturedImages));
     }
 
     function updateGallery() {
-        // Clear the current content of the gallery container
         galleryContainer.innerHTML = '';
 
-        // Display captured images in the updated gallery container
         capturedImages.forEach((imageDataURL, index) => {
             const imageElement = createImageElement(imageDataURL, index);
             galleryContainer.appendChild(imageElement);
